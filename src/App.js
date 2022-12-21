5cc358baf6661c9b8f3186946891fa45d251165f
marcoscastig <1castiglionimarcos@gmail.com>
2022-12-21 18:53:57 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import NavarMenu from './Components/NavarMenu';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+//import { useState } from 'react';
+//import { useEffect } from 'react';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavarMenu/>
+    </>
   );
 }
 
---
+++ src/Components/NavarMenu.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+export default function NavarMenu() {
+  
+    
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Link a la pagina de GitHub: <a href="https://github.com/marcoscastig" target='_blank' rel="noreferrer" >Marcos Castiglioni</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
